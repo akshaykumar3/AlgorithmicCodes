c14ff17639be78433002af90bcd6e6032b78ff72
localadmin <localadmin@Akshay.local>
2017-02-11 12:40:21 +0530

    Addding main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/* https://www.hackerrank.com/challenges/journey-to-the-moon */
+using namespace std;
+#include<stdio.h>
+#include<iostream>
+#include<queue>
+#include<vector>
+#define MAX 2147483647
+#define p pair<int,int>
+#define max(a,b) (a>b?a:b)
+#define min(a,b) (a>b?b:a)
+#define get(a) (scanf("%d",&a))
+#define N 100000
+bool vis[N];
+int connected[N];
+vector<int> v[N];
+
+int main() {
+    int n,i,m,j,x,y,count;
+    get(n);
+    get(m);
+    cout<<vis[0];
+}
